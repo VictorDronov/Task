087ad46daa8af80d6d8f47cce7c97b876b55585f
Maksim V <11082252+maks112v@users.noreply.github.com>
2021-09-10 15:21:07 -0400

    basic auth

---
+++ basicallydo/lib/realm.ts
@@ -0,0 +1,4 @@
+import * as Realm from "realm-web";
+
+export const realmApp: Realm.App = new Realm.App({ id: "tasks-kndvk" });
+
--- basicallydo/pages/index.tsx
+++ basicallydo/pages/index.tsx
@@ -1,43 +1,23 @@
-import type { NextPage } from "next";
-import { useEffect, useState } from "react";
-import { CreateTaskForm, Tasks } from "../components";
-import { useRouter } from "next/router";
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { CreateTaskForm, Tasks } from '../components';
 
 const Home: NextPage = () => {
   const [isSignedIn, setSignedIn] = useState(false);
   const router = useRouter();
 
-  const check = () => {
-    if (localStorage.getItem("Token")) {
-      setSignedIn(true);
-    } else {
-      router.push("/auth");
-    }
-  };
-
-  useEffect(() => {
-    check();
-  }, []);
-
-  const logOut = () => {
-    localStorage.removeItem("Token");
-    setSignedIn(false);
-    router.push("/auth");
-  };
-
   return (
     <div>
-      {isSignedIn && (
-        <>
-          <button onClick={() => logOut()}>Log Out</button>
-          <div className="flex flex-col justify-center text-center">
-            <h1 className="mt-3 font-extrabold">Task</h1>
-            <CreateTaskForm />
-            <h2 className="mt-6 mb-6 font-semibold">Your Tasks</h2>
-            <Tasks />
-          </div>
-        </>
-      )}
+      <>
+        {/* <button>Log Out</button> */}
+        <div className='flex flex-col justify-center text-center'>
+          <h1 className='mt-3 font-extrabold'>Task</h1>
+          <CreateTaskForm />
+          <h2 className='mt-6 mb-6 font-semibold'>Your Tasks</h2>
+          <Tasks />
+        </div>
+      </>
     </div>
   );
 };
