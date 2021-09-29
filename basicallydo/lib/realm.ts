5dfa996dff6570a5f2ff57979698dc7348e8fa03
victor <victordronov1@gmail.com>
2021-09-28 20:04:29 -0400

    test fix

--- basicallydo/lib/realm.ts
+++ basicallydo/lib/realm.ts
@@ -1,7 +1,7 @@
 import * as Realm from "realm-web";
 
 export const realmApp: Realm.App = new Realm.App({
-  id: process.env.NEXT_PUBLIC_APP_ID,
+  id: process.env.NEXT_PUBLIC_APP_ID || "",
 });
 
 export const mongodb = realmApp?.currentUser?.mongoClient("mongodb-atlas");
