8217ae587920ca1f24555cac6fc5972dee38fe3b
victor <victordronov1@gmail.com>
2021-10-22 17:59:22 -0400

    File name change

--- task/components/common/contentContainer.tsx
+++ task/components/common/contentContainer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Footer from "./Footer";
-import Header from "./Header";
+import Footer from "./FooterSlice";
+import Header from "./HeaderSlice";
 
 interface IProps {
   header?: boolean;
